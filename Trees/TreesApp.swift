5c6913257d6f7e0e01b40b380fc89213e17a8259
Issif Diawara <issifdiawarapro@gmail.com>
2023-02-24 16:29:29 +0100

    Initial Commit

---
+++ Trees/TreesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreesApp.swift
+//  Trees
+//
+//  Created by Issif DIAWARA on 24/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct TreesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
